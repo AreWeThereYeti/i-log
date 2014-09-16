589448be93d7c6f3fde159f9632ba10220d9fcb9
jonaslading <j_lading@hotmail.com>
2014-09-16 17:19:26 +0200

    replaces ID with Log.ID

--- app/scripts/controllers/LogCtrl.js
+++ app/scripts/controllers/LogCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller('LogCtrl', [ 'logs', '$routeParams', function (logs, $routeParams) {
+app.controller('LogCtrl', ['$window', '$scope', 'logs', '$routeParams', function ($window, $scope, logs, $routeParams) {
 	//Save reference to controller in order to avoid reference soup
 	var Log = this;
 
@@ -8,4 +8,17 @@ app.controller('LogCtrl', [ 'logs', '$routeParams', function (logs, $routeParams
 	Log.route = $routeParams.id;
 	//Test variable. If you see it when the app runs you are good to go
 	Log.testVar = 'We are up and running  on Log - page!';
+
+  $window.Math.max
+
+  // function binding 'formula' number scope models
+  Log.parseFormula = function(formula){
+    var exp;
+    // needs loop that
+    exp = formula.replace(/ID.(?!=\\.)/i, Log.ID1);
+    exp = exp.replace(/ID.(?!=\\.)/i, Log.ID2);
+    return exp;
+  }
+
+
 }]);
