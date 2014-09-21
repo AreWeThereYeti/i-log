75f27c9e8eeb2e7fdb1111c28701412b43b6c354
jonaslading <j_lading@hotmail.com>
2014-09-21 17:35:09 +0200

    highest, lowest, average and sum functions working

--- app/scripts/controllers/LogCtrl.js
+++ app/scripts/controllers/LogCtrl.js
@@ -1,6 +1,6 @@
 "use strict";
 
-app.controller('LogCtrl', ['$window', '$scope', 'logs', '$routeParams', function ($window, $scope, logs, $routeParams) {
+app.controller('LogCtrl', ['$scope', 'logs', '$routeParams', function ($scope, logs, $routeParams) {
 	//Save reference to controller in order to avoid reference soup
 	var Log = this;
   Log.test = logs.data.inputs;
@@ -64,6 +64,13 @@ app.controller('LogCtrl', ['$window', '$scope', 'logs', '$routeParams', function
     return sum;
   };
 
+/*  //  formats TIMEFORMAT(ID1, ‘hh:mm:ss’)
+  Log.timeFormat = function(val){
+    var time = "";
+
+
+  };*/
+
   // function binding 'formula' number scope models
   Log.parseFormula = function(formula){
     var exp = formula;
@@ -73,24 +80,43 @@ app.controller('LogCtrl', ['$window', '$scope', 'logs', '$routeParams', function
       exp = exp.replace(re, Log.numFields[i].value);
     }
 
+    for(var i=0; i<Log.numFields.length; i++){
+      if(angular.isUndefined(Log.numFields[i].value)) {
+        // Expression returned if number fields are not defined
+        return exp;
+      }
+    }
+
+    // TIMEFORMAT
+/*    if(exp.search(/TIMEFORMAT\(/g)!=-1){
+
+      if(exp.search(/TIMEFORMAT\(([^\(]+)(?=\))/i)!=-1){ // findes der et HIGHER(...) udtryk (ikke HIGHER(...()...))
+        var func = exp.match(/TIMEFORMAT\(([^\(]+)(?=\))/ig);
+        func[0] = func[0].replace(/TIMEFORMAT\(/g, "");
+
+        var num = [];
+        num = Log.parseElements(func[0], num);
+        exp = exp.replace(/TIMEFORMAT\(([^\(]+)\)/ig, Log.timeFormat(num));
+      }else{
+        // HIGHEST function includes invalid parenthesises. ex  FUNC( (ID1*100), ID2)
+        // return either empty string or error
+        return exp;
+
+      }
+    }*/
+
+
     // Checks for HIGHEST formula. returns math.max(exp) only if all number fields are filled
     if(exp.search(/HIGHEST\(/g)!=-1){
 
-      if(exp.search(/HIGHEST\(([^\(]+)(?=\))/i)!=-1){ // findes der et HIGHER(...) udtryk (ikke HIGHER(...()...))
-        var func = exp.match(/HIGHEST\(([^\(]+)(?=\))/ig);//.slice(0,8);
+      if(exp.search(/HIGHEST\(([^\(]+)(?=\))/i)!=-1){
+        var func = exp.match(/HIGHEST\(([^\(]+)(?=\))/ig);
         func[0] = func[0].replace(/HIGHEST\(/g, "");
-        //exp = exp.replace(/HIGHEST\(/g, "").slice(0,-1);
-        for(var i=0; i<Log.numFields.length; i++){
-          if(angular.isUndefined(Log.numFields[i].value)) {
-            // Expression returned if number fields are not defined
-            return exp;
-          }
-        }
         var num = [];
         num = Log.parseElements(func[0], num);
         exp = exp.replace(/HIGHEST\(([^\(]+)\)/ig, Log.highest(num));
       }else{
-        // HIGHEST function includes invalid parenthesises. ex  HIGHEST( (ID1*100), ID2)
+        // HIGHEST function includes invalid parenthesises. ex  FUNC( (ID1*100), ID2)
         // return either empty string or error
         return exp;
 
@@ -99,51 +125,50 @@ app.controller('LogCtrl', ['$window', '$scope', 'logs', '$routeParams', function
 
     // Checks for LOWEST formula. returns math.min(exp) only if all number fields are filled
     if(exp.search(/LOWEST\(/g)!=-1){
-      exp = exp.replace(/LOWEST\(/g, "").slice(0,-1);
-      for(var i=0; i<Log.numFields.length; i++){
-        if(angular.isUndefined(Log.numFields[i].value)) {
-          // Expression returned if number fields are not defined
-          return exp;
-        }
+
+      if(exp.search(/LOWEST\(([^\(]+)(?=\))/i)!=-1){
+        var func = exp.match(/LOWEST\(([^\(]+)(?=\))/ig);
+        func[0] = func[0].replace(/LOWEST\(/g, "");
+        var num = [];
+        num = Log.parseElements(func[0], num);
+        exp = exp.replace(/LOWEST\(([^\(]+)\)/ig, Log.lowest(num));
+      }else{
+        // LOWEST function includes invalid parenthesises. ex  FUNC( (ID1*100), ID2)
+        // return either empty string or error
+        return exp;
+
       }
-      var num = [];
-      num = Log.parseElements(exp, num);
-      return Log.lowest(num);
     }
 
     // Checks for AVERAGE formula. returns math.mean.mean(exp) only if all number fields are filled
     if(exp.search(/AVERAGE\(/g)!=-1){
-      exp = exp.replace(/AVERAGE\(/g, "").slice(0,-1);
-      for(var i=0; i<Log.numFields.length; i++){
-        if(angular.isUndefined(Log.numFields[i].value)) {
-          // Expression returned if number fields are not defined
-          return exp;
-        }
+      if(exp.search(/AVERAGE\(([^\(]+)(?=\))/i)!=-1){
+        var func = exp.match(/AVERAGE\(([^\(]+)(?=\))/ig);
+        func[0] = func[0].replace(/AVERAGE\(/g, "");
+        var num = [];
+        num = Log.parseElements(func[0], num);
+        exp = exp.replace(/AVERAGE\(([^\(]+)\)/ig, Log.average(num));
+      }else{
+        // AVERAGE function includes invalid parenthesises. ex  FUNC( (ID1*100), ID2)
+        // return either empty string or error
+        return exp;
+
       }
-      var num = [];
-      num = Log.parseElements(exp, num);
-      return Log.average(num);
     }
 
     // Checks for SUm formula. returns math.sum(exp) only if all number fields are filled
     if(exp.search(/SUM\(/g)!=-1){
-      exp = exp.replace(/SUM\(/g, "").slice(0,-1);
-      for(var i=0; i<Log.numFields.length; i++){
-        if(angular.isUndefined(Log.numFields[i].value)) {
-          // Expression returned if number fields are not defined
-          return exp;
-        }
-      }
-      var num = [];
-      num = Log.parseElements(exp, num);
-      return Log.sum(num);
-    }
-
-
-    // Returns evaluated expression only if all number fields are filled
-    for(var i=0; i<Log.numFields.length; i++){
-      if(angular.isUndefined(Log.numFields[i].value)) {
+      if(exp.search(/SUM\(([^\(]+)(?=\))/i)!=-1){
+        var func = exp.match(/SUM\(([^\(]+)(?=\))/ig);
+        func[0] = func[0].replace(/SUM\(/g, "");
+        var num = [];
+        num = Log.parseElements(func[0], num);
+        exp = exp.replace(/SUM\(([^\(]+)\)/ig, Log.sum(num));
+      }else{
+        // SUM function includes invalid parenthesises. ex  FUNC( (ID1*100), ID2)
+        // return either empty string or error
         return exp;
+
       }
     }
 
