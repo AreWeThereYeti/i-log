25f7cd2528a9b45cb70e6471f716e2e098a52ca0
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-09-21 22:13:23 +0200

    dialog is workin on report page

--- app/scripts/controllers/MenubarCtrl.js
+++ app/scripts/controllers/MenubarCtrl.js
@@ -1,13 +1,13 @@
 "use strict";
 
-app.controller('MenubarCtrl', [ function () {
+app.controller('MenubarCtrl', ['ngDialog', function (ngDialog) {
 	//Save reference to controller in order to avoid reference soup
 	var Menubar = this;
 
-	Menubar.modalShown = false;
-	Menubar.toggleModal = function() {
-		console.log('working')
-		Menubar.modalShown = !Menubar.modalShown;
+	Menubar.clickToOpen = function () {
+		ngDialog.open({
+			template: 'scripts/templates/dialog.html'
+		});
 	};
 
 	//Test variable. If you see it when the app runs you are good to go
--- app/scripts/controllers/RapportCtrl.js
+++ app/scripts/controllers/RapportCtrl.js
@@ -4,14 +4,6 @@ app.controller('RapportCtrl', ['report', 'ngDialog', function (report, ngDialog)
 	//Save reference to controller in order to avoid reference soup
 	var Rapport = this;
 
-	Rapport.clickToOpen = function () {
-		console.log('click to open')
-		ngDialog.open({
-			template: 'scripts/templates/dialog.html',
-			controller : 'DialogCtrl'
-		});
-	};
-
 	Rapport.data = {
 		"regions": ["Federal", "Tigray", "Afar", "Amhara", "Oromia", "Gambella", "Addis Ababa", "Dire Dawa", "Harar", "Benishangul-Gumuz", "Somali", "SNNPR "],
 		"institutions": [0, 0, 34, 421, 738, 0, 218, 22, 22, 109, 0, 456]
