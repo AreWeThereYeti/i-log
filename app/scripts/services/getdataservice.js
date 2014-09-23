dff524ddbdb58d185576d853532dac3419c169cc
Mikkel Markvardsen <mikkelmarkvardsen@Mikkels-MacBook-Pro.local>
2014-09-23 14:26:11 +0200

    no changes

--- app/scripts/services/getdataservice.js
+++ app/scripts/services/getdataservice.js
@@ -3,7 +3,6 @@ angular.module('gyldendal.services', [])
 			var sdo= {
 				getAllLogs: function () {
 					var promise = $http({cache: false, method: 'GET', url: 'http://www.mocky.io/v2/5412cb2886a645eb0aa1c336' }).success(function (data, status, headers, config) {
-
 					});
 					return promise;
 				},
