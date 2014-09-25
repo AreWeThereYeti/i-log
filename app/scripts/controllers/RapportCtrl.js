4da4f2ceaf6378928ff47fed3f8fd231aa72ea96
jonaslading <j_lading@hotmail.com>
2014-09-25 15:01:47 +0200

    list view importing values from dummydata

--- app/scripts/controllers/RapportCtrl.js
+++ app/scripts/controllers/RapportCtrl.js
@@ -20,33 +20,150 @@ app.controller('RapportCtrl', ['report', function (report) {
     }
 	];
 
+  Rapport.dummylogs = [
+    {
+      "timestamp":1411645441874,
+      "data":
+      {
+        "1":"Noget tekst",
+        "2":28,
+        "3":"datetime",
+        "4":14,
+        "5":"test",
+        "6":true,
+        "7":"LAAANG tekst"
+      }
+    },
+    {
+      "timestamp":1411645646885,
+      "data":
+      {
+        "1":"Noget tekst2",
+        "2":10,
+        "3":"datetime",
+        "4":5,
+        "5":"test",
+        "6":true,
+        "7":"LAAANG tekst"
+      }
+    },
+    {
+      "timestamp":1411645441874,
+      "data":
+      {
+        "1":"Noget tekst3",
+        "2":8,
+        "3":"datetime",
+        "4":1,
+        "5":null,
+        "6":true,
+        "7":"LAAANG tekst"
+      }
+    }
+
+  ];
 
   Rapport.listdata = [
     {
-      "type": "list",
-      "title": "list title",
-      "columns": [
+      "id":1,
+      "type":"list",
+      "title":"report title",
+      "columns":[
         {
-          "id": 1, // ID'et på feltet som tildelt i rapporten
-          "inputID": 2, // er dette id en reference til en tal række? Hvor kommer kolonne inholdet fra?
-          "label": "Kolonnetitel1"
+          "id":"K1",
+          "inputID":1,
+          "label":"number"
         },
         {
-          "id": 2, // ID'et på feltet som tildelt i rapporten
-          "inputID": 1,
-          "label": "Kolonnetitel2"
+          "id":"K2",
+          "inputID":2,
+          "label":"33"
         }
       ],
-      "calculations": [
+      "calculations":[
         {
-          "title": "Beregningstitel",
-          "formula": "SUM",
-          "columns": [
-            1,
-            2
+          "label":"Total",
+          "formula":"SUM",
+          "unit":"km",
+          "columns":[
+            "K1",
+            "K2"
+          ]
+        },
+        {
+          "label":"Gennemsnit x2",
+          "formula":"AVERAGE*2",
+          "unit":"km",
+          "columns":[
+            "K1"
           ]
         }
       ]
+    },
+    {
+      "id":1410335640193,
+      "title":"Test af ny rapport type",
+      "type":"diagram",
+      "views":
+      {
+        "piechart":true,
+        "barchart":true
+      },
+      "chart":
+      {
+        "domain":
+        {
+          "input":null,
+          "title":"hej"
+        },
+        "value":
+        {
+          "formula":"min formel",
+          "title":"hej2"
+        },
+        "calculations":[
+          {
+            "label":"calcTitel",
+            "formula":"calc1",
+            "unit":"m"
+          }
+        ]
+      }
+    },
+    {
+      "id":1410596140878,
+      "title":"Min vilde gra",
+      "type":"graph",
+      "views":
+      {
+        "scatterPlot":true,
+        "connectedGraph":false
+      },
+      "chart":
+      {
+        "xAxis":
+        {
+          "title":"X akse label",
+          "showDate":true
+        },
+        "yAxis":
+        {
+          "inputID":"3",
+          "title":"Y-akse label som jeg har valgt"
+        },
+        "calculations":[
+          {
+            "label":"Sum af 1 + 2",
+            "formula":"SUM(1+2)",
+            "unit":"m"
+          },
+          {
+            "label":"Sum af 3 + 4",
+            "formula":"SUM(3+4)",
+            "unit":"km"
+          }
+        ]
+      }
     }
   ];
 
@@ -104,4 +221,23 @@ app.controller('RapportCtrl', ['report', function (report) {
 
 	//Test variable. If you see it when the app runs you are good to go
 	Rapport.testVar = 'We are up and running  on rapports overview -page!';
+
+  // Function for parsing column calculations in list view
+
+  var listFormula = function(formula){
+
+    // determine columns used un formula
+
+    // gather all affected column entries in an array
+
+    // use regex to evaluate the formula function type
+
+    // use statcalcservice helper functions to replace formula function with evaluated expression
+
+    // return expression evaluated with $eval
+  };
+
+
+
+
 }]);
