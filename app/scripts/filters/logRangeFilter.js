ee584425d3baae07a6eb7d4212e38438870f99b4
jonaslading <j_lading@hotmail.com>
2014-10-14 15:42:50 +0200

    adds log filter in log view

--- app/scripts/filters/logRangeFilter.js
+++ app/scripts/filters/logRangeFilter.js
@@ -7,7 +7,7 @@ angular.module('gyldendal.filters', [])
           var two_days_ago = new Date().getTime() - range * 86400000;
 
           // objects[i].timestamp should match the data format for logs
-          var last_modified = new Date(objects[i].timestamp * 1000).getTime();
+          var last_modified = new Date(objects[i].timestamp).getTime();
           if (two_days_ago <= last_modified) {
             filtered_list.push(objects[i]);
           }
