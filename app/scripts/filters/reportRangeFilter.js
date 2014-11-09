0c7ae7a1d8cad3ad86dd54c4dc86d061ee9301c8
jonaslading <j_lading@hotmail.com>
2014-11-09 21:09:24 +0100

    maps report dropdown filter to from date

--- app/scripts/filters/reportRangeFilter.js
+++ app/scripts/filters/reportRangeFilter.js
@@ -5,7 +5,7 @@ angular.module('gyldendal.filters')
       if(range) {
         for (var i = 0; i < objects.length; i++) {
           var two_days_ago = new Date().getTime() - range * 86400000;
-          var last_modified = new Date(objects[i].from * 1000).getTime();
+          var last_modified = new Date(objects[i].content.from).getTime();
           if (two_days_ago <= last_modified) {
             filtered_list.push(objects[i]);
           }
