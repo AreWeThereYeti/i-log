18a704092e91ce2e49511e01567de174bfe86944
jonaslading <j_lading@hotmail.com>
2014-11-18 21:50:59 +0100

    fix for log prompt on new log

--- app/scripts/controllers/LogMenubarCtrl.js
+++ app/scripts/controllers/LogMenubarCtrl.js
@@ -9,7 +9,22 @@ app.controller('LogMenubarCtrl', ['$routeParams', 'getdataservice', '$rootScope'
 
   LogMenubar.checkInputs = function(){
 
-    // checks if all required fields are filled, alerts and returns if not
+    // if new log entry, check if fields has been filled before discarding these
+    if(!angular.isDefined($scope.$parent.Log.route)){
+      angular.forEach($scope.$parent.Log.inputs.length, function(){
+        if(angular.isDefined($scope.$parent.Log.inputs[i].value)){
+          if(confirm("Der er foretaget ikke-gemte indtastninger. Disse vil gå tabt hvis du fortsætter.")){
+            $location.path('logs');
+            return;
+          } else {
+            return;
+          }
+        }
+      });
+      $location.path('logs');
+      return;
+    }
+    // checks if fields has been changed, and alerts before such changes are discarded
     for(var i=0; i<$scope.$parent.Log.inputs.length; i++){
       if(angular.isDefined($scope.$parent.Log.inputs[i].value)){
         if($scope.$parent.Log.inputs[i].value != $scope.$parent.Log.currentLog.data[$scope.$parent.Log.inputs[i].id]){
