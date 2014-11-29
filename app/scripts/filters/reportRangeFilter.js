a5622a136df69a8eec74b050713e54386b024793
Mikkel Markvardsen <mikkelmarkvardsen@gmail.com>
2014-11-29 21:09:27 +0100

    better timefilter

--- app/scripts/filters/reportRangeFilter.js
+++ app/scripts/filters/reportRangeFilter.js
@@ -4,9 +4,14 @@ angular.module('gyldendal.filters')
       var filtered_list = [];
       if(range) {
         for (var i = 0; i < objects.length; i++) {
-          var two_days_ago = new Date().getTime() - range * 86400000;
+          var one_day = 24*60*60*1000;
+          var today = new Date().getTime();
+
           var last_modified = new Date(objects[i].content.from).getTime();
-          if (two_days_ago <= last_modified) {
+
+          var diffDays = Math.round(Math.abs((today - last_modified)/(one_day)));
+
+          if (diffDays < last_modified) {
             filtered_list.push(objects[i]);
           }
         }
