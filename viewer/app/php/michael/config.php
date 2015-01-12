b48927c58082300f55de66b0a623ff56c1e690e8
Mads Felskov Agersten <mads@felskov-aps.dk>
2015-01-12 22:50:15 +0100

    Fix environment check to support staging URLs

--- viewer/app/php/michael/config.php
+++ viewer/app/php/michael/config.php
@@ -8,7 +8,7 @@ define('GYLDENDAL_COMPONENTBRIDGE_SECRET', '327d5df2-bc8a-4233-b550-8089ac8a6e10
 //Time in minutes when a signature expires
 define('LOGIN_EXPIRE_TIME', 100000000);
 
-if ($_SERVER['HTTP_HOST'] === 'local.components.gyldendal.dk' || $_SERVER['HTTP_HOST'] === 'stage2.sl-udv.dk' || $_SERVER['HTTP_HOST'] === 'jonasyazid.dk' || $_SERVER['HTTP_HOST'] === 'localhost:8888' || $_SERVER['HTTP_HOST'] === 'clients.envisage.dk') {
+if ($_SERVER['HTTP_HOST'] === 'gyldendal.felskov.io' || $_SERVER['HTTP_HOST'] === 'gyldendal.local.io' || $_SERVER['HTTP_HOST'] === 'local.components.gyldendal.dk' || $_SERVER['HTTP_HOST'] === 'stage2.sl-udv.dk' || $_SERVER['HTTP_HOST'] === 'jonasyazid.dk' || $_SERVER['HTTP_HOST'] === 'localhost:8888' || $_SERVER['HTTP_HOST'] === 'clients.envisage.dk') {
 	define('GYLDENDAL_COMPONENTAPI_URL', 'http://bridge.test.components.gyldendal.dk/api/');   
 }
 else {
